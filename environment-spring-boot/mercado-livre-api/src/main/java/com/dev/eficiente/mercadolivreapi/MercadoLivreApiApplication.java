99ba1f76b59b03d0b7687931f28c97eb17648c76
Valdir Junior <junior_vs@outlook.com>
2021-03-13 18:11:27 -0300

    validacao do email do autor e categoria

---
+++ environment-spring-boot/mercado-livre-api/src/main/java/com/dev/eficiente/mercadolivreapi/MercadoLivreApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.eficiente.mercadolivreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLivreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoLivreApiApplication.class, args);
+	}
+
+}
---
+++ environment-spring-boot/mercado-livre-api/src/test/java/com/dev/eficiente/mercadolivreapi/MercadoLivreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.eficiente.mercadolivreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
